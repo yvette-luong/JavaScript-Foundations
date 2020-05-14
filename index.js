3a99108475301b01263295735e0fa04db1976a5b
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-05-13 23:07:15 -0700

    yvette-luong

--- index.js
+++ index.js
@@ -132,9 +132,11 @@ For example, variableInterestRate(200000, 0.04, 30) should console.log:
 "{Name}, with an interest rate of 0.055, your monthly rate is $1136"
 "{Name}, with an interest rate of 0.06, your monthly rate is $1199"
 */
+function  variableInterestRate (P,N,I)
 
-
-
+let principal    = P ;
+let numerator    = N ;
+let interestRate = I;
 
 // 🌟🌟🌟 STRETCH 🌟🌟🌟//
 
